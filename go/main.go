319918e973992319c688f38b17fa6709185c42b4
jgarcia <2745523+jmichelgarcia@users.noreply.github.com>
2018-12-14 11:27:50 +0000

    refactor: using new template, refactor file serve as well

--- go/main.go
+++ go/main.go
@@ -8,28 +8,24 @@ import (
 )
 
 type index_vars struct {
-	Hostname string
+	Instance_id string
+	Instance_index string
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 
-	hostname, err := os.Hostname()
+    instance_id := os.Getenv("CF_INSTANCE_GUID")
+	instance_index := os.Getenv("CF_INSTANCE_INDEX")
 	template_file, err :=  ioutil.ReadFile("index.html.template")
 	if err != nil { panic(err) }
         tmpl, err := template.New("goose").Parse(string(template_file))
 	if err != nil { panic(err) }
-	err = tmpl.Execute(w, index_vars{Hostname: hostname})
+	err = tmpl.Execute(w, index_vars{Instance_id: instance_id, Instance_index: instance_index})
 	if err != nil { panic(err) }
 }
 
-func GooseHandler(w http.ResponseWriter, r *http.Request) {
-	data, _ := ioutil.ReadFile("goose.jpg")
-        w.Write(data)
-}
-
-
 func main() {
 	http.HandleFunc("/", IndexHandler)
-	http.HandleFunc("/static/goose.jpg", GooseHandler)
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 	http.ListenAndServe(":8080", nil)
 }
